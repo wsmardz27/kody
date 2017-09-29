663ce91d5e311a95b62a43b5efa8f7e851b6bfdc
wsmardz27 <wiktoriasmardz@gmail.com>
2017-09-29 11:41:57 +0200

    kalkulatori trojkat

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -8,22 +8,30 @@
 
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(int argc, char **argv)
 { 
     int a, b, c;
+    int obwod = 0;
+    float p = 0;
     a = b = c = 0;
-    cout << "Podaj 3 boki: ";
+    cout << "Podaj 3 boki: " << endl;
     cin >> a >> b >> c;
     
     if (a + b > c && b + c > a && a + c > b)
     {
-    cout << "Trójkąt można zbudować " << endl;
-    cout << "Obwód trójkąta = " << a + b + c;
+        cout << "Trójkąt można zbudować " << endl;
+        obwod = a + b + c;
+        cout << "Obwód trójkąta = " << obwod << endl;
+        p = 0.5 * obwod;
+        cout << "p = " << p << endl;
+        cout <<"Pole trójkąta = " << sqrt (p * (p-a)*(p-b)*(p-c)) << endl;
 
 }
+  
 	return 0;
 }
 
