d3659731ff553891f98ac67ba405c3351ae4a80d
wsmardz27 <wiktoriasmardz@gmail.com>
2017-10-13 11:39:42 +0200

    pętle switch

---
+++ cpp/petle_znak.cpp
@@ -0,0 +1,24 @@
+/*
+ * petle_switch.cpp
+ * Program pobiera znak zn typu char do momentu, gdy jest on literą 't' 'T' 'n' 'N'
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+
+{
+    char zn='';
+    
+     while () {
+    cout << "Podaj miesiąc (1-12)" << endl;
+    cin >> m; 
+    };
+    
+    
+    return 0;
+}
+
