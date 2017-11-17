949bc07a1f5456d0eae0bb0e22624346d28c8d61
wsmardz27 <wiktoriasmardz@gmail.com>
2017-11-17 11:41:23 +0100

    silnia

--- Python/potega.py
+++ Python/potega.py
@@ -9,18 +9,29 @@ def potega_it(podst, wykladnik):
     """Funkcja oblicza iteracyjnie potęgę liczby naturalnej"""
     wynik = 1
     for i in range(wykladnik):
-        wynik = wynik*i
+        wynik = wynik * podst
     return wynik
 
 def main(args):
-    # pobierz od użytkownika podstawę i wykładnik 
+    # pobierz od użytkownika podstawę i wykładnik
     # i przypisz do odpowiednich zmiennych
-    # wywołaj funkcje potęga_it() 
-    int(input("Podaj podstawę: "))
-    int(input("Podaj wykładnik: "))
+    # wywołaj funkcje potega_it()
+
+    a = int(input("Podaj podstawę: "))
+    n = int(input("Podaj wykładnik: "))
+    assert type(a) == int
+    assert type(n) == int
+
+    assert potega_it(10, 0) == 1
+    assert potega_it(10, 1) == 10
+    assert potega_it(2, 3) == 8
+
+
+    # print("Wynik: ", potega_it(a, n))
+
     return 0
 
-    
+
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
