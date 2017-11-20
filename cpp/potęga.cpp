4c4aba6a2344d6e715a530ac96b20d6980c3b949
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2017-11-20 21:47:21 +0100

    silnia

---
+++ cpp/potęga.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int podstawa;
+    int wykladnik;
+    int wynik = 0;
+
+    cout << "Podaj podstawê : ";
+    cin >> podstawa;
+    cout <<"Podaj wyk³adnik: " ;
+    cin >> wykladnik;
+
+   
+
+
+    return 0;
+}
---
+++ cpp/silnia.cpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int liczba;
+    int silnia = 1;
+
+    cout << "Podaj liczbe" << endl;
+    cin >> liczba;
+
+    for (int i = 1; i<=liczba;i++)
+        silnia *= i;
+
+    if (liczba == 0)
+        cout << "1 " << endl;
+    else
+        cout << silnia << endl;
+    return 0;
+}
+
