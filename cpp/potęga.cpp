76dbda6d7b3b15f905c8054b7c444ecf3566dc61
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2017-11-20 22:24:05 +0100

    silnia i potęga

--- cpp/potęga.cpp
+++ cpp/potęga.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+# include  <cmath>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    int podstawa;
-    int wykladnik;
-    int wynik = 0;
+    int podstawa = 0;
+    int wykladnik = 0;
+    int wynik = 1;
 
-    cout << "Podaj podstawê : ";
+    cout << "Podaj podstawe : ";
     cin >> podstawa;
     cout <<"Podaj wyk³adnik: " ;
     cin >> wykladnik;
 
-   
+    for (int i = 0; i < wykladnik; i++)
+        wynik*=podstawa;
 
+    if (wykladnik == 0)
+        cout << "1" << endl;
+
+    else
+        cout << wynik << endl;
 
     return 0;
 }
