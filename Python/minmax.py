483ef544d237d3c770ff4e37f87a01948ba4466f
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2017-11-27 23:11:00 +0100

    minmax

--- Python/minmax.py
+++ Python/minmax.py
@@ -20,19 +20,22 @@ def maksimum(lista):
 
 
 def minmax(lista):
-    lmin = []
     lmax = []
+    lmin = []
     indeks = 0
+
     for i in range(int(len(lista) / 2)):
         if lista[indeks] > lista[indeks + 1]:
-            indeks += 2
-            lmin.append(lista[indeks +1])
             lmax.append(lista[indeks])
+            lmin.append(lista[indeks + 1])
         else:
+            lmax.append(lista[indeks+1])
             lmin.append(lista[indeks])
-            lmax.append(lista[indeks + 1])
+        indeks += 2
+    print("Lista max: ", lmax)
+    print("Lista min: ", lmin)
 
-
+    return lmax, lmin
 
 
 def losuj(ile, zakres):
@@ -49,6 +52,7 @@ def main(args):
     assert minimum([7, 4, 9, 1, 3, 0]) == 0
     assert maksimum([7, 4, 9, 1, 3, 0]) == 9
     print(lista)
+    minmax(lista)
     print("Minimum: ", minimum(lista))
     print("Maksimum: ", maksimum(lista))
     return 0
