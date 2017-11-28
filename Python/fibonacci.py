731035d7146588f887f7761a41d13951a6648e75
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2017-11-28 21:04:22 +0100

    ciąg Fibonacciego

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -2,7 +2,29 @@
 # -*- coding: utf-8 -*-
 
 
+def fib_iter(n):
+    """Funkcja wyświetla kolejne wyrazy ciągu Fibonacciego. Funkcja zwraca n-ty wyraz ciągu
+    F(0) = 0
+    F(1) = 1
+    F(n) = F(n-2) + F(n-1) dla n > 1 """
+    a, b = (0, 1)
+    if n == 0:
+        print (a)
+        return a
+    elif n == 1:
+        print (b)
+        return b
+    elif n > 1:
+        for i in range(1, n-1):
+            a, b = b, a + b
+            print (i + 2, b)
+            
+        return b
+
+
 def main(args):
+    n = int(input("Podaj nr miesiąca: "))
+    fib_iter(n)
     return 0
 
 
