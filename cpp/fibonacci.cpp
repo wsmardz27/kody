c78d60424d08f66910f0df385595a67e71cd661a
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2017-11-30 21:02:26 +0100

    ciąg Fibonacciego

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -8,20 +8,34 @@
 using namespace std;
 
 int fib_iter(int n){
-    int a = 0
-    int b = 0
+    int a = 0;
+    int b = 1;
     if (n == 0) {
         cout >> 0;
         return 0;
     }
     cout << a;
-    for(int i=2; i < n; i++)
+    for(int i=2; i < n; i++){
+        tmp = b
+        b = a + b
+        a = tmp
+
+        }
     }
     
 
+int fib_iter2(int n){
+    int a = 0;
+    int b = 1;
+    
+    while(n > 0){}
+    
+    }
+
+
 int main(int argc, char **argv)
 {
-	
+
 	return 0;
 }
 
