87ece22b1b3568e6c8633afe48473852d7676aec
nazwa_użytkownika <wiktoriasmardz@gmail.com>
2018-01-23 19:49:57 +0100

    alg

---
+++ Python/alg_lpierwsza.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  
+
+
+def main(args):
+    n = 0
+    n = int(input("Podaj liczbę: "))
+    
+    i = 2
+    while n % i != 0:
+        if i * i > n:
+            print("Liczba pierwsza")
+        i += 1
+    print("Liczba złożona")
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
