a86071611b32102e52d900d3f28dadc9b5887f02
wsmardz27 <wiktoriasmardz@gmail.com>
2018-01-23 13:28:44 +0100

    algorytmy

---
+++ Smardz_alg1.cpp
@@ -0,0 +1,33 @@
+/*
+ * Smardz_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a;
+    
+    cout << "Podaj liczbe: " << endl;
+    cin >> a;
+    
+    if (a < 0 || a > 100)
+    cout << "Podaj liczbe: " << endl;
+    cin >> a;
+    
+    
+    for(int i = 1; i = 100; i++){
+        i = 2;
+        if (a == i)
+            cout << "parzysta" << endl;
+        else
+            i = i + 2
+}
+    
+	return 0;
+}
+
+// algorytm nie jest poprawny
